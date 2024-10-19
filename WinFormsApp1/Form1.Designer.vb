08fc424d8676bd3e194d794ae24082f8c1fa5c8f
king leonard <king leonard@LAPTOP-ELA8Q5KH>
2024-10-19 17:50:24 +0800

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtBookID = New TextBox()
+        txtDays = New TextBox()
+        btnRent = New Button()
+        btnReturn = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txtBookID
+        ' 
+        txtBookID.Location = New Point(261, 245)
+        txtBookID.Name = "txtBookID"
+        txtBookID.Size = New Size(125, 27)
+        txtBookID.TabIndex = 0
+        ' 
+        ' txtDays
+        ' 
+        txtDays.Location = New Point(436, 245)
+        txtDays.Name = "txtDays"
+        txtDays.Size = New Size(125, 27)
+        txtDays.TabIndex = 1
+        ' 
+        ' btnRent
+        ' 
+        btnRent.Location = New Point(279, 295)
+        btnRent.Name = "btnRent"
+        btnRent.Size = New Size(94, 29)
+        btnRent.TabIndex = 2
+        btnRent.Text = "Rent"
+        btnRent.UseVisualStyleBackColor = True
+        ' 
+        ' btnReturn
+        ' 
+        btnReturn.Location = New Point(451, 295)
+        btnReturn.Name = "btnReturn"
+        btnReturn.Size = New Size(94, 29)
+        btnReturn.TabIndex = 3
+        btnReturn.Text = "Return"
+        btnReturn.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(261, 26)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(300, 188)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(btnReturn)
+        Controls.Add(btnRent)
+        Controls.Add(txtDays)
+        Controls.Add(txtBookID)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtBookID As TextBox
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents btnRent As Button
+    Friend WithEvents btnReturn As Button
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+    Public Class Book
+
+
+        Public Property BookID As Integer
+        Public Property Title As String
+        Public Property Author As String
+        Public Property Available As Boolean
+        Public Property DueDate As DateTime
+
+        Dim bookList As New List(Of Book)
+
+
+        Private Sub Form1_Load(sender As Object, e As EventArgs, dataGridView1 As DataGridView) Handles MyBase.Load
+            ' Add sample books
+            bookList.Add(New Book() With {.BookID = 1, .Title = "Harry Potter", .Author = "J.K. Rowling", .Available = True})
+            bookList.Add(New Book() With {.BookID = 2, .Title = "The Hobbit", .Author = "J.R.R. Tolkien", .Available = True})
+
+            UpdateBookGrid(dataGridView1)
+        End Sub
+
+
+        Private Sub UpdateBookGrid(dataGridView1 As DataGridView)
+            dataGridView1.DataSource = Nothing
+            dataGridView1.DataSource = bookList
+        End Sub
+
+        Private Sub RentBook(bookID As Integer, days As Integer, dataGridView1 As DataGridView)
+            Dim book = bookList.FirstOrDefault(Function(b) b.BookID = bookID)
+            If book IsNot Nothing AndAlso book.Available Then
+                book.Available = False
+                book.DueDate = DateTime.Now.AddDays(days)
+                UpdateBookGrid(dataGridView1)
+                MessageBox.Show($"Book '{book.Title}' rented. Due on {book.DueDate.ToShortDateString()}.")
+            Else
+                MessageBox.Show("Book not available for rent.")
+            End If
+        End Sub
+
+        Private Sub ReturnBook(bookID As Integer, dataGridView1 As DataGridView)
+            Dim book = bookList.FirstOrDefault(Function(b) b.BookID = bookID)
+            If book IsNot Nothing AndAlso Not book.Available Then
+                book.Available = True
+                book.DueDate = DateTime.MinValue
+                UpdateBookGrid(dataGridView1)
+                MessageBox.Show($"Book '{book.Title}' returned.")
+            Else
+                MessageBox.Show("Book not currently rented.")
+            End If
+        End Sub
+
+#Disable Warning IDE0051 ' Remove unused private members
+        Private Sub BtnRent_Click(sender As Object, e As EventArgs, txtDays As TextBox, txtBookID As TextBox, dataGridView1 As DataGridView) Handles btnRent.Click
+
+#Enable Warning IDE0051 ' Remove unused private members
+
+            Dim bookID As Integer = CInt(txtBookID.Text)
+            Dim days As Integer = CInt(txtDays.Text)
+            RentBook(bookID, days, dataGridView1)
+        End Sub
+
+        Private Sub BtnReturn_Click(sender As Object, e As EventArgs, txtBookID As TextBox, dataGridView1 As DataGridView) Handles btnReturn.Click
+
+
+            Dim bookID As Integer = CInt(txtBookID.Text)
+            ReturnBook(bookID, dataGridView1)
+        End Sub
+    End Class
+End Class
+
+
